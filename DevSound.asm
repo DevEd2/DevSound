e319d3537b119fa9370939ba3715bceccfa2f8b5
Panda Habert <eldredhabert0@gmail.com>
2017-10-18 05:21:01 +0200

    Fix "pop" when fading in/out
    That's because GlobalVolume can be set by Init before Fade can ; now, Play sets the initial sound instead - which makes Fade a very trivial function.

--- DevSound.asm
+++ DevSound.asm
@@ -29,13 +29,6 @@ UseFXHammer	set	0
 ; wave volume scaling, channel volume
 ; DemoSceneMode = 1
 
-; Uncomment this if you just want to disable wave volume scaling
-; NoWaveVolumeScaling = 1
-
-; Uncomment this to only disable zombie mode (for
-; compatibility with lesser emulators such as VBA).
-; DisableZombieMode=1
-
 DevSound:
 
 include	"DevSound_Vars.asm"
@@ -174,31 +167,11 @@ DevSound_Stop:
 
 DevSound_Fade:
 	and	3
-	or	a
-	jr	z,.fadeOut
-	dec	a
-	jr	z,.fadeIn
-	dec	a
-	jr	z,.fadeOutStop
-	ret	; default case
-.fadeOut
-	inc	a
+	cp	3
+	ret	z ; 3 is an invalid value, silently ignore it
+	inc	a ; Increment...
+	set	2,a ; Mark this fade as the first
 	ld	[FadeType],a
-	add	6
-	ld	[GlobalVolume],a
-	jr	.done
-.fadeIn
-	ld	a,2
-	ld	[FadeType],a
-	xor	a
-	ld	[GlobalVolume],a
-	jr	.done
-.fadeOutStop
-	ld	a,3
-	ld	[FadeType],a
-	add	4
-	ld	[GlobalVolume],a
-.done
 	ld	a,7
 	ld	[FadeTimer],a
 	ret
@@ -630,8 +603,6 @@ CH2_CheckByte:
 .odd
 	call	CH2_SetInstrument
 .noInstrumentChange
-	ld	hl,CH2Reset
-	set	7,[hl]
 	jp	UpdateCH3
 	
 .endChannel
@@ -650,11 +621,11 @@ CH2_CheckByte:
 .nullnote
 	ld	a,[hl+]
 	dec	a
-	ld	[CH2Tick],a		; set tick
+	ld	[CH1Tick],a		; set tick
 	ld	a,l				; store back current pos
-	ld	[CH2Ptr],a
+	ld	[CH1Ptr],a
 	ld	a,h
-	ld	[CH2Ptr+1],a
+	ld	[CH1Ptr+1],a
 	jp	UpdateCH3
 	
 .getCommand
@@ -949,11 +920,11 @@ CH3_CheckByte:
 .nullnote
 	ld	a,[hl+]
 	dec	a
-	ld	[CH3Tick],a
+	ld	[CH2Tick],a
 	ld	a,l				; store back current pos
-	ld	[CH3Ptr],a
+	ld	[CH2Ptr],a
 	ld	a,h
-	ld	[CH3Ptr+1],a
+	ld	[CH2Ptr+1],a
 	jp	UpdateCH4
 	
 .getCommand
@@ -1256,7 +1227,6 @@ CH4_CheckByte:
 	ld	[CH4Ptr+1],a
 	xor	a
 	ld	[CH4NoisePos],a
-	ld	[CH4WavePos],a
 	ld	a,[CH4Reset]
 	bit	1,a
 	jr	nz,.noresetvol
@@ -1307,11 +1277,11 @@ CH4_CheckByte:
 .nullnote
 	ld	a,[hl+]
 	dec	a
-	ld	[CH4Tick],a
+	ld	[CH2Tick],a
 	ld	a,l				; store back current pos
-	ld	[CH4Ptr],a
+	ld	[CH2Ptr],a
 	ld	a,h
-	ld	[CH4Ptr+1],a
+	ld	[CH2Ptr+1],a
 	jp	DoneUpdating
 	
 .getCommand
@@ -1460,10 +1430,6 @@ CH4_SetInstrument:
 	ld	[CH4NoisePtr],a
 	ld	a,[hl+]
 	ld	[CH4NoisePtr+1],a
-	ld	a,[hl+]
-	ld	[CH4WavePtr],a
-	ld	a,[hl+]
-	ld	[CH4WavePtr+1],a
 	ret
 	
 ; ================================================================
@@ -1495,9 +1461,24 @@ UpdateRegisters:
 
 	; update global volume + fade system
 	ld	a,[FadeType]
-	and	3
-	or	a
+	ld	b,a
+	and	3 ; Check if no fade
 	jr	z,.updateVolume ; Update volume
+	
+	bit	2,b ; Check if on first fade
+	jr	z,.notfirstfade
+	res	2,b
+	ld	a,b
+	ld	[FadeType],a
+	ld	b,a
+	dec a ; If fading in (value 1), volume is 0 ; otherwise, it's 7
+	jr	z,.gotfirstfadevolume
+	ld	a,7
+.gotfirstfadevolume
+	ld	[GlobalVolume],a
+	ld	a,b
+.notfirstfade
+	
 	ld	a,[FadeTimer]
 	and	a
 	jr	z,.doupdate
@@ -1508,12 +1489,22 @@ UpdateRegisters:
 	ld	a,7
 	ld	[FadeTimer],a
 	ld	a,[FadeType]
+	and 3
 	dec	a
 	jr	z,.fadeout
 	dec	a
 	jr	z,.fadein
 	dec	a
-	jr	z,.fadeoutstop
+	jr	nz,.updateVolume
+.fadeoutstop
+	ld	a,[GlobalVolume]
+	and	a
+	jr	z,.dostop
+	dec	a
+	ld	[GlobalVolume],a
+	jr	.directlyUpdateVolume
+.dostop
+	call	DevSound_Stop
 	jr	.updateVolume
 .fadeout
 	ld	a,[GlobalVolume]
@@ -1532,16 +1523,6 @@ UpdateRegisters:
 .done
 	xor	a
 	ld	[FadeType],a
-	jr	.updateVolume
-.fadeoutstop
-	ld	a,[GlobalVolume]
-	and	a
-	jr	z,.dostop
-	dec	a
-	ld	[GlobalVolume],a
-	jr	.directlyUpdateVolume
-.dostop
-	call	DevSound_Stop
 .updateVolume
 	ld	a,[GlobalVolume]
 .directlyUpdateVolume ; Call when volume is already known
@@ -1570,14 +1551,6 @@ CH1_UpdateRegisters:
 
 	; update arps
 .updatearp
-; Deflemask compatibility: if pitch bend is active, don't update arp and force the transpose of 0
-	ld	a,[CH1PortaType]
-	and	a
-	jr	z,.noskiparp
-	xor	a
-	ld	[CH1Transpose],a
-	jr	.continue
-.noskiparp
 	ld	hl,CH1ArpPtr
 	ld	a,[hl+]
 	ld	h,[hl]
@@ -1961,13 +1934,6 @@ CH2_UpdateRegisters:
 
 	; update arps
 .updatearp
-	ld	a,[CH2PortaType]
-	and	a
-	jr	z,.noskiparp
-	xor	a
-	ld	[CH2Transpose],a
-	jr	.continue
-.noskiparp
 	ld	hl,CH2ArpPtr
 	ld	a,[hl+]
 	ld	h,[hl]
@@ -2368,13 +2334,6 @@ CH3_UpdateRegisters:
 
 	; update arps
 .updatearp
-	ld	a,[CH3PortaType]
-	and	a
-	jr	z,.noskiparp
-	xor	a
-	ld	[CH3Transpose],a
-	jr	.continue
-.noskiparp
 	ld	hl,CH3ArpPtr
 	ld	a,[hl+]
 	ld	h,[hl]
@@ -2625,13 +2584,13 @@ if !def(DemoSceneMode)
 endc
 	ld	a,[CH3Vol]
 	cp	b
-if !def(DemoSceneMode) && !def(NoWaveVolumeScaling)
+if !def(DemoSceneMode)
 	ld	a,0
 endc
 	jr	z,.noreset3
 	ld	a,b
 	ld	[CH3Vol],a
-if def(DemoSceneMode) || def(NoWaveVolumeScaling)
+if def(DemoSceneMode)
 	and	a
 	ld	b,a
 	jr	z,.skip
@@ -2649,7 +2608,7 @@ if def(DemoSceneMode) || def(NoWaveVolumeScaling)
 	ld	a,b
 	ld	[CH3ComputedVol],a
 	ld	[rNR32],a
-	ld	a,d
+	ld	a,e
 	or	$80
 	ldh	[rNR34],a
 .noreset3
@@ -2685,30 +2644,19 @@ endc
 	ld	b,a
 	ld	a,[CH3Wave]
 	cp	b
-if def(DemoSceneMode) || def(NoWaveVolumeScaling)
+if def(DemoSceneMode)
 	jr	z,.noreset2
 	ld	a,b
 	ld	[CH3Wave],a
-	cp	$c0
-	push	hl
-if def(DemoSceneMode) 
-	jr	z,.noreset2			; if value = $c0, ignore (since this feature is disabled in DemoSceneMode)
-else
-	ld	hl,WaveBuffer
-	jr	z,.wavebuf
-endc
+	cp	$c0					; if value = $c0, ignore (since this feature is disabled in DemoSceneMode)
+	jr	z,.noreset2
 	ld	c,b
 	ld	b,0
 	ld	hl,WaveTable
 	add	hl,bc
 	add	hl,bc
-	ld	a,[hl+]
-	ld	h,[hl]
-	ld	l,a
-.wavebuf
 	call	LoadWave
-	pop	hl
-	ld	a,d
+	ld	a,e
 	or	%10000000
 	ldh	[rNR34],a
 .noreset2
@@ -2736,7 +2684,6 @@ endc
 if !def(DemoSceneMode)
 	call	DoPWM
 	call	DoRandomizer
-if !def(NoWaveVolumeScaling)
 	ld	a,[WaveBufUpdateFlag]
 	and	a
 	jp	z,.noupdate
@@ -2837,7 +2784,6 @@ if !def(NoWaveVolumeScaling)
 	ldh	[rNR34],a
 .noupdate
 endc
-endc
 
 ; ================================================================
 
@@ -2900,55 +2846,12 @@ CH4_UpdateRegisters:
 	inc	a
 	ld	[CH4NoisePos],a
 .continue
-
-	; update wave
-	ld	hl,CH4WavePtr
-	ld	a,[hl+]
-	ld	h,[hl]
-	ld	l,a
-	ld	a,[CH4WavePos]
-	add	l
-	ld	l,a
-	jr	nc,.nocarry3
-	inc	h
-.nocarry3
-	ld	a,[hl+]
-	cp	$ff
-	jr	z,.updateNote
-	ld	[CH4Wave],a
-	ld	a,[CH4WavePos]
-	inc	a
-	ld	[CH4WavePos],a
-	ld	a,[hl+]
-	cp	$fe
-	jr	nz,.updateNote
-	ld	a,[hl]
-	ld	[CH4WavePos],a
 	
 ; get note
 .updateNote
-	ld	a,[CH4Mode]
-	ld	b,a
 	ld	a,[CH4Transpose]
-	bit	7,a
-	jr	nz,.minus
-	add	b
-	cp	45
-	jr	c,.noclamp
-	ld	a,44
-	jr	.noclamp
-.minus
-	add	b
-	cp	45
-	jr	c,.noclamp
-	xor	a
-.noclamp
 	ld	b,a
-	ld	a,[CH4Wave]
-	and	a
-	jr	z,.noise15
-	ld	a,45
-.noise15
+	ld	a,[CH4Mode]
 	add	b
 	
 	ld	hl,NoiseTable
@@ -3071,7 +2974,7 @@ DoneUpdatingRegisters:
 ; ================================================================
 
 LoadWave:
-if !def(DemoSceneMode) && !def(NoWaveVolumeScaling)
+if !def(DemoSceneMode)
 	ld	hl,ComputedWaveBuffer
 endc
 	ldh	a,[rNR51]
@@ -3470,8 +3373,8 @@ DefaultRegTable:
 	dw	DummyTable,DummyTable,DummyTable,DummyTable,DummyTable
 	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 	; ch4
-	dw	DummyTable,DummyTable,DummyTable,DummyTable
-	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+	dw	DummyTable,DummyTable,DummyTable
+	db	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 	
 DefaultWave:	db	$01,$23,$45,$67,$89,$ab,$cd,$ef,$fe,$dc,$ba,$98,$76,$54,$32,$10
 
@@ -3482,7 +3385,6 @@ NoiseData:		incbin	"NoiseData.bin"
 ; ================================================================
 	
 DummyTable:	db	$ff
-vib_Dummy:	db	0,0,$80,1
 
 DummyChannel:
 	db	EndChannel
