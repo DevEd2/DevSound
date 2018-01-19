0c45626dbe4e09bd11d8275009027c71af93aa95
DevEd2 <deved8@gmail.com>
2018-01-18 22:52:42 -0500

    update copyright year + removed my full name

--- DevSound.asm
+++ DevSound.asm
@@ -1,7 +1,7 @@
 ; ================================================================
 ; DevSound - a Game Boy music system by DevEd
 ;
-; Copyright (c) 2017 Edward J. Whalen
+; Copyright (c) 2017 - 2018 DevEd
 ; 
 ; Permission is hereby granted, free of charge, to any person obtaining
 ; a copy of this software and associated documentation files (the
